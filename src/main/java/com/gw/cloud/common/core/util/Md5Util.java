88f86dc02b00c183d2ddfaba08fed446a7ff90d5
chenligang <13833008589@qq.com>
2019-07-20 18:05:12 +0800

    增加 MD5

--- src/main/java/com/gw/cloud/common/core/util/Md5Util.java
+++ src/main/java/com/gw/cloud/common/core/util/Md5Util.java
@@ -7,6 +7,8 @@ import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.DESKeySpec;
 import javax.crypto.spec.IvParameterSpec;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * MD5工具类
@@ -26,6 +28,44 @@ public class Md5Util {
      * 解密KEY
      */
     private static final String DECRYPT_KEY = "1q2a3z4w";
+
+
+
+    /***
+     * 生成32位md5码
+     * @param password
+     * @return
+     */
+    public static String md5Password(String password) {
+
+        try {
+            // 得到一个信息摘要器
+            MessageDigest digest = MessageDigest.getInstance("md5");
+            byte[] result = digest.digest(password.getBytes());
+            StringBuffer buffer = new StringBuffer();
+            // 把每一个byte 做一个与运算 0xff;
+            for (byte b : result) {
+                // 与运算
+                int number = b & 0xff;// 加盐
+                String str = Integer.toHexString(number);
+                if (str.length() == 1) {
+                    buffer.append("0");
+                }
+                buffer.append(str);
+            }
+
+            // 标准的md5加密后的结果
+            return buffer.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return "";
+        }
+
+    }
+
+
+
+
 
     /**
      * 解密
