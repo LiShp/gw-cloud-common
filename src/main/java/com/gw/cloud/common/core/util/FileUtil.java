b346858a40ddf93229ce726f5c876d121c6214b0
dingcuicui <123@456.com>
2019-09-02 14:14:42 +0800

    文件上传下载

--- src/main/java/com/gw/cloud/common/core/util/FileUtil.java
+++ src/main/java/com/gw/cloud/common/core/util/FileUtil.java
@@ -1,8 +1,10 @@
 package com.gw.cloud.common.core.util;
 
 import com.gw.cloud.common.core.base.exception.ApplicationException;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -61,6 +63,26 @@ public class FileUtil {
         bufferedWriter.write(text);
         bufferedWriter.close();
         writerStream.close();
+    }
+
+    /**
+     * 将上传的文件写入缓存文件并保存
+     *
+     * @param filePath 文件路径
+     * @param file     上传文件
+     * @return 缓存文件
+     * @throws IOException IO异常
+     */
+    public static File uploadFile(String filePath, MultipartFile file) throws IOException {
+        // 得到文件对象
+        File fileCache = new File(filePath+ StringUtil.STR_FILE_SEPARATOR + file.getOriginalFilename());
+        // 判断文件所在的目录是否存在，若不存在则创建
+        if (!fileCache.getParentFile().exists()) {
+            fileCache.getParentFile().mkdirs();
+        }
+        // 写文件
+        file.transferTo(fileCache);
+        return fileCache;
     }
 
     /**
@@ -135,6 +157,52 @@ public class FileUtil {
         }
     }
 
-
+    /**
+     * 文件下载
+     * @param response
+     * @param fileName
+     */
+    public static void download( HttpServletResponse response, String filePath, String fileName){
+        //通过文件的保存文件夹路径加上文件的名字来获得文件
+        //File file=new File("/data/",fileName);
+        File file = new File(filePath+ StringUtil.STR_FILE_SEPARATOR + fileName);
+        //当文件存在
+        if(file.exists()){
+            //首先设置响应的内容格式是force-download，那么你一旦点击下载按钮就会自动下载文件了
+            response.setContentType("application/force-download");
+            //通过设置头信息给文件命名，也即是，在前端，文件流被接受完还原成原文件的时候会以你传递的文件名来命名
+            response.addHeader("Content-Disposition",String.format("attachment; filename=\"%s\"", file.getName()));
+            //进行读写操作
+            byte[] buffer=new byte[1024];
+            FileInputStream fis=null;
+            BufferedInputStream bis=null;
+            try{
+                fis=new FileInputStream(file);
+                bis=new BufferedInputStream(fis);
+                OutputStream os=response.getOutputStream();
+                //从源文件中读
+                int i=bis.read(buffer);
+                while(i!=-1){
+                    //写到response的输出流中
+                    os.write(buffer,0,i);
+                    i=bis.read(buffer);
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }finally {
+                //善后工作，关闭各种流
+                try {
+                    if(bis!=null){
+                        bis.close();
+                    }
+                    if(fis!=null){
+                        fis.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
 }
